3bd2f9a8223b7405f6366bddf543dffae5d4d589
Chris Williams <cawilliams@iqms.com>
2017-08-30 11:25:57 -0700

    Initial checkin

---
+++ .tmp/precompile/visualPlugin.ts
@@ -0,0 +1,11 @@
+module powerbi.visuals.plugins {
+    export var PBI_CV_885EF3C3_31C1_4745_B2B9_20771D5AD196_DEBUG = {
+        name: 'PBI_CV_885EF3C3_31C1_4745_B2B9_20771D5AD196_DEBUG',
+        displayName: 'PowerBI DataImage',
+        class: 'Visual',
+        version: '1.0.0',
+        apiVersion: '1.3.0',
+        create: (options: extensibility.visual.VisualConstructorOptions) => new powerbi.extensibility.visual.PBI_CV_885EF3C3_31C1_4745_B2B9_20771D5AD196.Visual(options),
+        custom: true
+    };
+}
